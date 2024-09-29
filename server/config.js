1df35a89a416b8daf6ca652510042816e707d652
WtOrWoT <wtorwot@gmail.com>
2024-09-29 17:03:00 +0500

    Update config.js

--- server/config.js
+++ server/config.js
@@ -6,7 +6,7 @@ module.exports = {
     host: "koolyras.glitch.me",
 
     // Which port to run the web server on.
-    port: 26301,
+    port: 3000,
 
     // How often to update the list of the entities that players can see.
     // Has effects of when entities are activated.
